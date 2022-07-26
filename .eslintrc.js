72264ea0e455510c31a84fe909af0ac74803250a
Bruno Guimaraes <devgmrs@gmail.com>
2022-07-26 11:35:07 -0300

    alteracao para desenvolvimento no WSL

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,14 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: ['airbnb-base', 'prettier'],
-	plugins: ['prettier'],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	rules: {
-		'import/prefer-default-export': 'off',
-		'react/state-in-constructor': 'off',
-		'react/static-property-placement': 'off',
-		'react/jsx-props-no-spreading': 'off',
-		'react/prop-types': 'off',
-		'no-param-reassign': 'off',
-		'no-console': 'off',
-		camelcase: 'off',
-		'class-methods-use-this': 'off',
-	},
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
 };
